0e334b16ec79e14c0275795bf91332d21e916d28
thomas lotsander <thomaslotsander@gmail.com>
2018-11-12 15:15:29 +0100

    Update TestRegisterStudents

--- StudentEnumrator.Tests/StudentTests.cs
+++ StudentEnumrator.Tests/StudentTests.cs
@@ -61,7 +61,7 @@ namespace StudentEnumrator.Tests
         {
             // Arrange
             var studentRepository = new StudentRepository();
-            int expectedCount = 6;
+            int expectedCount = 7;
 
             // Act
             Program.RegisterStudents(studentRepository);
