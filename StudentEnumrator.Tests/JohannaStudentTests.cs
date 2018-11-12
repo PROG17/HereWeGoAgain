854d69abb7e64a0cdd750b02f38555beb1c4f1a6
johanna åkerström <johanna_akerstrom@hotmail.com>
2018-11-12 15:05:30 +0100

    add johannastudent

---
+++ StudentEnumrator.Tests/JohannaStudentTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentEnumerator;
+
+namespace StudentEnumrator.Tests
+{
+    [TestClass]
+    public class JohannaStudentTests
+    {
+        [TestMethod]
+        public void TestSayGoodmorning()
+        {
+            // Arrange
+            var student = new JohannaStudent();
+
+            // Act
+            var result = student.SayGoodmorning();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
