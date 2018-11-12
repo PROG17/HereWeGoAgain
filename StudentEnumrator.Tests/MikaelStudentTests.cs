ce053c2c1094c61e34fba9229691ed4c43ad384e
Mikael Hietala <Mikael.Hietala@yh.nackademin.se>
2018-11-12 15:04:30 +0100

    Added MikaelStudent and tests

---
+++ StudentEnumrator.Tests/MikaelStudentTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentEnumerator;
+
+namespace StudentEnumrator.Tests
+{
+    [TestClass]
+    public class MikaelStudentTests
+    {
+
+        [TestMethod]
+        public void TestSayGoodmorning()
+        {
+            // Arrange
+            var student = new MikaelStudent();
+
+            // Act
+            var result = student.SayGoodmorning();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
