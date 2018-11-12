f42adc2627de3850b05c3ce07b9c64e611299a30
Joakim Fröjd <frojdie@gmail.com>
2018-11-12 15:15:46 +0100

    Added Uttern

---
+++ StudentEnumerator/UtternStudent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEnumerator
+{
+    public class UtternStudent : Person
+    {
+        public override string SayGoodmorning()
+        {
+            return @"Uttern är ett smidigt, 
+långsmalt djur med korta ben, brett huvud och lång, muskulös svans. 
+Pälsen är ljusbrun med en nästan vit haklapp. Den är starkt anpassad för ett liv i vatten";
+        }
+    }
+}
---
+++ StudentEnumrator.Tests/UtternStudentTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentEnumerator;
+
+namespace StudentEnumrator.Tests
+{
+    [TestClass]
+    public class UtternStudentTests
+    {
+        [TestMethod]
+        public void TestSayGoodmorning()
+        {
+            // Arrange
+            var student = new UtternStudent();
+            var expected = @"Uttern är ett smidigt, 
+långsmalt djur med korta ben, brett huvud och lång, muskulös svans. 
+Pälsen är ljusbrun med en nästan vit haklapp. Den är starkt anpassad för ett liv i vatten";
+
+            // Act
+            var result = student.SayGoodmorning();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
